0566222e5ac268b724689ed692248bbde0657742
m4thz12 <rm98832@fiap.com.br>
2023-05-23 11:44:32 -0300

    desgraçaaaa

---
+++ js/sucesso.js
@@ -0,0 +1,23 @@
+
+    
+    if(JSON.parse(localStorage.getItem("user-token")) != null){
+        //const userBemVindo = document.querySelector("#userWelcome");
+        let usuario = JSON.parse(localStorage.getItem("user-validado"));
+        
+        const legenda = document.querySelector("#figure-img figcaption");
+        const img = document.querySelector("#img-avatar");
+
+        img.src = usuario.avatarUsuario;
+        img.alt = usuario.nomeCompleto;
+        
+        legenda.textContent = usuario.nomeCompleto +" - "+ usuario.nomeUsuario;
+        // userBemVindo.innerHTML = usuario.nomeCompleto;
+
+        const botaoLogout = document.querySelector("#btnLogout");
+        botaoLogout.addEventListener("click", ()=>{
+            localStorage.removeItem("user-token");
+            window.location.href = "../login.html";
+        });
+    }else{
+        window.location.href = "../login.html";
+    }
--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -145,3 +145,5 @@ inputUser.addEventListener("keyup", ()=>{
     }
 });
 
+
+
